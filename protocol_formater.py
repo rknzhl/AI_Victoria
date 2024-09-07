d685c1f8cbd93a4c3b31d16c152fe62bb20d2a0c
Danil Kadyrov <121870156+dankadyrov@users.noreply.github.com>
2024-09-07 22:15:34 +0300

    Add files via upload

--- protocol_formater.py
+++ protocol_formater.py
@@ -1,22 +1,31 @@
-""" To run this code:
-   brew install texlive
-   brew install pandoc
-   and imported python libraries
+""" Переводит ответ LLM модели из .json файла в .docx или .pdf
+    с определенными параметрами
 """
 
 import datetime
 import json
+import subprocess
+import os
 
 from docx import Document
 from docx.shared import Pt, Inches, RGBColor
-import pypandoc
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 
 template_path = "template.docx"
-yandex_gpt_answer = 'unofficial_answer.json'
 
 
-def create_informal_docx(yandex_gpt_answer: str, output_path: str) -> None:
+def create_informal_docx(yandex_gpt_answer: str, output_path: str, timestamps=False, context=False) -> None:
+    """
+    Формирование .docx файла с неформальным отчетом о встрече
+
+    yandex_gpt_answer: путь до .json файла с ответом YaGPT
+    output_path: путь до файла, где сохранится .docx файл. Требует указание полного пути и имени файла
+    timestamps: включать в отчет временные промежутки реплик или нет
+    context: включать в отчет вонтекст обждения или нет
+
+    Returns: None
+    """
     with open(yandex_gpt_answer, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
@@ -114,31 +123,199 @@ def create_informal_docx(yandex_gpt_answer: str, output_path: str) -> None:
         run_1.italic = True
         list_paragraph_1.paragraph_format.space_after = Pt(6)
 
-        list_paragraph_2 = doc.add_paragraph()
-        run_2 = list_paragraph_2.add_run(
-            f"           Контекст обсуждения: {data["Тезисы"][idx]["Контекст обсуждения"]}")
-        list_paragraph_2.paragraph_format.left_indent = 360000  # Отступ для маркера
-        list_paragraph_2.paragraph_format.first_line_indent = -360000  # Отступ для текста
-        list_paragraph_2.paragraph_format.space_after = Pt(6)
-        run_2.italic = True
-        run_2.font.color.rgb = RGBColor(68, 125, 194)
+        if context:
+            list_paragraph_2 = doc.add_paragraph()
+            run_2 = list_paragraph_2.add_run(
+                f"           Контекст обсуждения: {data["Тезисы"][idx]["Контекст обсуждения"]}")
+            list_paragraph_2.paragraph_format.left_indent = 360000  # Отступ для маркера
+            list_paragraph_2.paragraph_format.first_line_indent = -360000  # Отступ для текста
+            list_paragraph_2.paragraph_format.space_after = Pt(6)
+            run_2.italic = True
+            run_2.font.color.rgb = RGBColor(68, 125, 194)
 
-        list_paragraph_3 = doc.add_paragraph()
-        run_3 = list_paragraph_3.add_run(f"           Время: {data["Тезисы"][idx]["Таймкод"]}")
-        list_paragraph_3.paragraph_format.left_indent = 360000  # Отступ для маркера
-        list_paragraph_3.paragraph_format.first_line_indent = -360000  # Отступ для текста
-        list_paragraph_3.paragraph_format.space_after = Pt(6)
-        run_3.italic = True
-        run_3.font.color.rgb = RGBColor(68, 125, 194)
+        if timestamps:
+            list_paragraph_3 = doc.add_paragraph()
+            run_3 = list_paragraph_3.add_run(f"           Время: {data["Тезисы"][idx]["Таймкод"]}")
+            list_paragraph_3.paragraph_format.left_indent = 360000  # Отступ для маркера
+            list_paragraph_3.paragraph_format.first_line_indent = -360000  # Отступ для текста
+            list_paragraph_3.paragraph_format.space_after = Pt(6)
+            run_3.italic = True
+            run_3.font.color.rgb = RGBColor(68, 125, 194)
 
     doc.save(output_path)
 
-# pdf пока не работет
-def create_informal_pdf(yandex_gpt_answer: str, output_path: str) -> None:
+
+def create_informal_pdf(yandex_gpt_answer: str, output_path: str, timestamps=False, context=False) -> None:
+    """
+    Формирование .pdf файла с неформальным отчетом о встрече.
+
+    yandex_gpt_answer: путь до .json файла с ответом YaGPT
+    output_path: путь до файла, где сохранится .docx файл. Требует указание полного пути и имени файла
+    timestamps: включать в отчет временные промежутки реплик или нет
+    context: включать в отчет вонтекст обждения или нет
+
+    Returns: None
+    """
     docx_file = output_path[:-3] + "docx"
-    create_informal_docx(yandex_gpt_answer, docx_file)
-    output = pypandoc.convert_file(docx_file, 'pdf', outputfile=output_path)
+    create_informal_docx(yandex_gpt_answer, docx_file, timestamps, context)
+
+    shell_script = './convert_docx_to_pdf.sh'
+    result = subprocess.run(['bash', shell_script, docx_file], capture_output=True, text=True)
+    if os.path.exists(docx_file):
+        os.remove(docx_file)
+        print(f"Файл {docx_file} успешно удален.")
+    else:
+        print(f"Файл {docx_file} не существует.")
 
 
-create_informal_pdf(yandex_gpt_answer, "results/output.pdf")
 
+
+
+def create_formal_docx(yandex_gpt_answer: str, output_path: str, timestamps=False, context=False) -> None:
+    """
+        Формирование .docx файла с формальным отчетом о встрече
+
+        yandex_gpt_answer: путь до .json файла с ответом YaGPT
+        output_path: путь до файла, где сохранится .docx файл. Требует указание полного пути и имени файла
+        timestamps: включать в отчет временные промежутки реплик или нет
+        context: включать в отчет вонтекст обждения или нет
+
+        Returns: None
+        """
+    with open(yandex_gpt_answer, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Копирование шаблона неофициального
+    with open(template_path, 'rb') as src, open(output_path, 'wb') as dst:
+        dst.write(src.read())
+
+    font_name = "Times New Roman"
+
+    doc = Document()
+
+    protocol_name = data["Название протокола"].split(" ")
+    protocol_word = protocol_name[0].upper()
+    protocol_name = " ".join(protocol_name[1:])
+
+    protocol_paragraph = doc.add_paragraph()
+    run = protocol_paragraph.add_run(protocol_word)
+    run.font.name = font_name
+    run.font.size = Pt(14)
+    run.bold = True
+    protocol_paragraph.paragraph_format.space_before = Pt(8)
+    protocol_paragraph.paragraph_format.space_after = Pt(8)
+    protocol_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    title_paragraph = doc.add_paragraph()
+    run = title_paragraph.add_run(protocol_name)
+    run.font.name = font_name
+    run.font.size = Pt(14)
+    run.bold = True
+    title_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    line_paragraph = doc.add_paragraph()
+    line_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    line_paragraph.paragraph_format.left_indent = Pt(20)  # Отступ слева
+    line_paragraph.paragraph_format.right_indent = Pt(20)  # Отступ справа
+    run = line_paragraph.add_run("________________________________________________________")
+    run.font.name = font_name
+    run.font.size = Pt(14)
+
+    city_paragraph = doc.add_paragraph()
+    run = city_paragraph.add_run("Москва")
+    run.font.name = font_name
+    run.font.size = Pt(14)
+    run.bold = True
+    city_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+
+    # DATE
+    current_date = str(datetime.date.today()).replace('-', '.')
+    current_time = datetime.datetime.now().time()
+
+    date_paragraph = doc.add_paragraph()
+    run = date_paragraph.add_run(current_date + "№___________")
+    run.font.name = font_name
+    run.font.size = Pt(14)
+    date_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+
+    line_paragraph = doc.add_paragraph()
+    line_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    line_paragraph.paragraph_format.left_indent = Pt(20)  # Отступ слева
+    line_paragraph.paragraph_format.right_indent = Pt(20)  # Отступ справа
+    run = line_paragraph.add_run("________________________________________________________")
+    run.font.name = font_name
+    run.font.size = Pt(14)
+
+    theme_paragraph = doc.add_paragraph()
+    run = theme_paragraph.add_run(data["Тема"])
+    run.font.name = font_name
+    run.font.size = Pt(14)
+    theme_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    line_paragraph = doc.add_paragraph()
+    line_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    line_paragraph.paragraph_format.left_indent = Pt(20)  # Отступ слева
+    line_paragraph.paragraph_format.right_indent = Pt(20)  # Отступ справа
+    run = line_paragraph.add_run("________________________________________________________")
+    run.font.name = font_name
+    run.font.size = Pt(14)
+
+    for idx in range(len(data["Постановления/решения"])):
+        list_paragraph_1 = doc.add_paragraph()
+        run_1 = list_paragraph_1.add_run(f"{idx + 1}.     {data["Постановления/решения"][idx]["Формулировка"]}")
+        list_paragraph_1.paragraph_format.left_indent = 360000  # Отступ для маркера
+        list_paragraph_1.paragraph_format.first_line_indent = -360000  # Отступ для текста
+        list_paragraph_1.paragraph_format.space_after = Pt(6)
+        run_1.font.name = font_name
+        run_1.font.size = Pt(14)
+
+        if context:
+            list_paragraph_2 = doc.add_paragraph()
+            run_2 = list_paragraph_2.add_run(
+                f"        Контекст обсуждения: {data["Постановления/решения"][idx]["Контекст обсуждения"]}")
+            list_paragraph_2.paragraph_format.left_indent = 360000  # Отступ для маркера
+            list_paragraph_2.paragraph_format.first_line_indent = -360000  # Отступ для текста
+            list_paragraph_2.paragraph_format.space_after = Pt(6)
+            run_2.italic = True
+            run_2.font.color.rgb = RGBColor(68, 125, 194)
+            run_2.font.name = font_name
+            run_2.font.size = Pt(14)
+
+        if timestamps:
+            list_paragraph_3 = doc.add_paragraph()
+            run_3 = list_paragraph_3.add_run(f"        Время: {data["Постановления/решения"][idx]["Таймкод"]}")
+            list_paragraph_3.paragraph_format.left_indent = 360000  # Отступ для маркера
+            list_paragraph_3.paragraph_format.first_line_indent = -360000  # Отступ для текста
+            list_paragraph_3.paragraph_format.space_after = Pt(6)
+            run_3.italic = True
+            run_3.font.color.rgb = RGBColor(68, 125, 194)
+            run_3.font.name = font_name
+            run_3.font.size = Pt(14)
+
+
+    doc.save(output_path)
+
+
+
+def create_informal_pdf(yandex_gpt_answer: str, output_path: str, timestamps=False, context=False) -> None:
+    """
+    Формирование .pdf файла с формальным отчетом о встрече.
+
+    yandex_gpt_answer: путь до .json файла с ответом YaGPT
+    output_path: путь до файла, где сохранится .docx файл. Требует указание полного пути и имени файла
+    timestamps: включать в отчет временные промежутки реплик или нет
+    context: включать в отчет вонтекст обждения или нет
+
+    Returns: None
+    """
+    docx_file = output_path[:-3] + "docx"
+    create_formal_docx(yandex_gpt_answer, docx_file, timestamps, context)
+
+    shell_script = './convert_docx_to_pdf.sh'
+    result = subprocess.run(['bash', shell_script, docx_file], capture_output=True, text=True)
+    if os.path.exists(docx_file):
+        os.remove(docx_file)
+        print(f"Файл {docx_file} успешно удален.")
+    else:
+        print(f"Файл {docx_file} не существует.")
